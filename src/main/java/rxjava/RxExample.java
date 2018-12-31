fa445add2db2f1438cd93cf016b01942e0a225fa
Jingjing Duan <duanjingjing@gmail.com>
2018-12-30 21:59:52 -0800

    RxJava example

--- src/main/java/rxjava/RxExample.java
+++ src/main/java/rxjava/RxExample.java
@@ -1,5 +1,9 @@
 package rxjava;
 
+import io.reactivex.Flowable;
+
 public class RxExample {
-    public static void
+    public static void main(String... args) {
+        Flowable.fromArray(args).subscribe(s -> System.out.println("hello " + s + "!"));
+    }
 }
